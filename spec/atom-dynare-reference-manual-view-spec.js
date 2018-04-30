fe70405d30c167f5991efbc04a59c6406ba495a4
Daniel Coutinho <daniel.mar.coutinho@gmail.com>
2018-04-30 20:40:47 -0300

    Creation of the package

---
+++ spec/atom-dynare-reference-manual-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomDynareReferenceManualView from '../lib/atom-dynare-reference-manual-view';
+
+describe('AtomDynareReferenceManualView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
